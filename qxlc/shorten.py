0a508c10fb84ffd4326d2cdc8b1f7eb8f7e01378
Dabo Ross <daboross@daboross.net>
2014-05-23 14:28:14 -0700

    Some formatting changes, and add 'http://' if it isn't present on URLs

--- qxlc/shorten.py
+++ qxlc/shorten.py
@@ -8,6 +8,12 @@ from qxlc import app
 from qxlc.database import encode_id, decode_id, store_data, get_data, type_id
 
 valid_url = re.compile(
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+    r'(?::\d+)?'  # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+url_with_protocol = re.compile(
     r'^(?:http|ftp)s?://'  # http:// or https://
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain
     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
@@ -25,6 +31,8 @@ def action_short():
     url = params["url"]
     if not valid_url.match(url):
         return "Invalid URL", 400
+    if not url_with_protocol.match(url):
+        url = "http://{}".format(url)
     return "http://qx.lc/{}".format(encode_id(store_data("url", url)))
 
 

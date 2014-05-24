28fa8f659c5585cfad6624e044ce7ec346dc2370
Dabo Ross <daboross@daboross.net>
2014-05-24 00:12:51 -0700

    Re-add option for http://|https://|ftp:// to urls, just don't make it mandatory

--- qxlc/shorten.py
+++ qxlc/shorten.py
@@ -8,6 +8,7 @@ from qxlc import app
 from qxlc.database import encode_id, decode_id, store_data, get_data, type_id
 
 valid_url = re.compile(
+    r'^(?:(?:http|ftp)s?://)?'  # http:// or https://
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain
     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
     r'(?::\d+)?'  # optional port

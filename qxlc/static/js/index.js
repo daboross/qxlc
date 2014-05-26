1737b26b773152bb38c67a6a74972f3bd0621c80
Dabo Ross <daboross@daboross.net>
2014-05-26 16:25:49 -0700

    Redo style a bunch, and add a footer

--- qxlc/static/js/index.js
+++ qxlc/static/js/index.js
@@ -4,36 +4,34 @@ function submitLink() {
     $linkArea.val("");
 
     var $resultRow = $('<tr/>');
-    var $resultLeft = $('<p/>').text(original_url);
+    var $resultLeft = $('<p/>').text(truncateData(original_url));
     var $resultRight = $('<p>').text('Submitting...');
     $resultRow.append($('<td/>').append($resultLeft));
     $resultRow.append($('<td/>').append($resultRight));
     $('#results-table').prepend($resultRow);
 
-    var $request = $.ajax({
-                              url: "/api/shorten?" + $.param({"url": original_url}),
-                              type: "GET",
-                              dataType: "text"
-                          });
+    var $request = $.ajax(
+        {
+            url: "/api/shorten",
+            type: "POST",
+            data: {"url": original_url},
+            dataType: "text"
+        }
+    );
     $request.success(function (data, textStatus, jqXHR) {
         $('#results-div').show();
-        var $inner_html = $("<input/>").attr("type", "text").attr("class", "form-control").val(data);
-        var $new_html = $("<p/>").append($inner_html);
-        $resultRight.replaceWith($new_html);
-        $inner_html.click(function () {
-            select($inner_html);
+        var $resultInner = $("<input/>").attr("type", "text").attr("class", "form-control").val(data);
+        var $newResult = $("<p/>").append($resultInner);
+        $resultRight.replaceWith($newResult);
+        $resultInner.click(function () {
+            select($resultInner);
         });
     });
     $request.fail(function (data, textStatus, jqXHR) {
         $('#results-div').show();
-        var new_html = $("<p/>").text("Failed: " + data.responseText);
-        $resultRight.replaceWith(new_html);
+        var $newResult = $("<p/>").text("Failed: " + data.responseText);
+        $resultRight.replaceWith($newResult);
     });
-}
-
-function truncateData(data) {
-    data = data.split("\n")[0]
-    data = data.substring(0, Math.min(data.length, 10))
 }
 
 function submitPaste() {
@@ -48,28 +46,41 @@ function submitPaste() {
     $resultRow.append($('<td/>').append($resultRight));
     $('#results-table').prepend($resultRow);
 
-    var $request = $.ajax({
-                              url: "/api/paste",
-                              type: "POST",
-                              data: {"paste": data},
-                              dataType: "text"
-                          });
+    var $request = $.ajax(
+        {
+            url: "/api/paste",
+            type: "POST",
+            data: {"paste": data},
+            dataType: "text"
+        }
+    );
     $request.success(function (data, textStatus, jqXHR) {
         $('#results-div').show();
-        var $inner_html = $("<input/>").attr("type", "text").attr("class", "form-control").val(data);
-        var $new_html = $("<p/>").append($inner_html);
-        $resultRight.replaceWith($new_html);
-        $inner_html.click(function () {
-            select($inner_html);
+        var $resultInner = $("<input/>").attr("type", "text").attr("class", "form-control").val(data);
+        var $newResult = $("<p/>").append($resultInner);
+        $resultRight.replaceWith($newResult);
+        $resultInner.click(function () {
+            select($resultInner);
         });
     });
     $request.fail(function (data, textStatus, jqXHR) {
         $('#results-div').show();
-        var new_html = $("<p/>").text("Failed: " + data.responseText);
-        $resultRight.replaceWith(new_html);
+        var $newResult = $("<p/>").text("Failed: " + data.responseText);
+        $resultRight.replaceWith($newResult);
     });
 }
 
+function truncateData(long_data) {
+    var data = long_data;
+    if (data.indexOf("\n") > -1) {
+        data = data.split("\n")[0];
+    }
+    data = data.substring(0, Math.min(data.length, 10));
+    if (data !== long_data) {
+        data += "..."
+    }
+    return data
+}
 function select(element) {
     element.focus();
     element.select();

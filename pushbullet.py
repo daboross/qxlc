f2ee7bd980fb23c2f9d74347dbd0435bac40bc3a
Dabo Ross <daboross@daboross.net>
2014-05-28 15:04:01 -0700

    Update pushbullet to use requests

--- pushbullet.py
+++ pushbullet.py
@@ -1,115 +1,49 @@
-# Adapted version of pyPushBullet (by Azelphur?)
-# Original repository: https://github.com/Azelphur/pyPushBullet
-
-from urllib.request import Request, urlopen
-from base64 import b64encode
 import json
-import mimetypes
-import os
 
-HOST = "https://api.pushbullet.com/api"
+import requests
+from requests.auth import HTTPBasicAuth
 
-
-class PushBulletError():
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return self.value
-
-
-def _encode_multipart_formdata(fields, files):
-    """
-    http://mattshaw.org/news/multi-part-form-post-with-files-in-python/
-    """
-
-    def guess_type(file_name):
-        return mimetypes.guess_type(file_name)[0] or 'application/octet-stream'
-
-    boundary_constant = "----------bound@ry_$"
-    crlf_constant = '\r\n'
-    data_list = []
-    for key, value in fields.items():
-        data_list.append("--" + boundary_constant)
-        data_list.append('Content-Disposition: form-data; name="{}"'.format(key))
-        data_list.append("")
-        data_list.append(str(value))
-
-    for (key, filename, value) in files:
-        data_list.append("--" + boundary_constant)
-        data_list.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
-        data_list.append("Content-Type: %s" % (guess_type(filename)))
-        data_list.append("")
-        data_list.append(value)
-
-    data_list.append("--" + boundary_constant + "--")
-    data_list.append("")
-    body = crlf_constant.join(data_list)
-    content_type = "multipart/form-data; boundary={}".format(boundary_constant)
-    return content_type, body
+HOST = "https://api.pushbullet.com/v2/"
 
 
 class PushBullet():
     def __init__(self, api_key):
         self.api_key = api_key
+        self._headers = {"Accept": "application/json", "Content-type": "application/json",
+                         "User-Agent": "qxlc (http://qx.lc)"}
+        self._args = {'headers': self._headers, 'auth': HTTPBasicAuth(self.api_key, ""), 'verify': True}
 
-    def _request(self, url, postdata=None):
-        request = Request(url)
-        request.add_header("Accept", "application/json")
-        request.add_header("Content-type", "application/json")
-        auth = "{}:".format(self.api_key)
-        auth = auth.encode('ascii')
-        auth = b64encode(auth)
-        auth = b"Basic " + auth
-        request.add_header("Authorization", auth)
-        request.add_header("User-Agent", "pyPushBullet")
-        if postdata:
-            postdata = json.dumps(postdata)
-            postdata = postdata.encode('utf-8')
-        response = urlopen(request, postdata)
-        data = response.read()
-        data = data.decode("utf-8")
-        j = json.loads(data)
-        return j
+    def push_note(self, title, body):
+        data = {'type': "note", 'title': title, 'body': body}
+        r = requests.post(HOST + "pushes", json.dumps(data), **self._args)
+        return r
 
-    def _request_multiform(self, url, postdata, files):
-        request = Request(url)
-        content_type, body = _encode_multipart_formdata(postdata, files)
-        request.add_header("Accept", "application/json")
-        request.add_header("Content-type", content_type)
-        auth = "{}:".format(self.api_key)
-        auth = auth.encode("ascii")
-        auth = b64encode(auth)
-        auth = b"Basic " + auth
-        request.add_header("Authorization", auth)
-        request.add_header("User-Agent", "pyPushBullet")
-        response = urlopen(request, body)
-        data = response.read()
-        data = data.decode("utf-8")
-        j = json.loads(data)
-        return j
+    def push_address(self, name, address, device=None):
+        data = {'type': 'address', 'name': name, 'address': address}
+        if device:
+            data["device_iden"] = device
+        r = requests.post(HOST + "pushes", json.dumps(data), **self._args)
+        return r.json()
 
-    def get_devices(self):
-        return self._request(HOST + "/devices")["devices"]
+    def push_list(self, title, items, device=None):
+        data = {'type': 'list', 'title': title, 'items': items}
+        if device:
+            data["device_iden"] = device
+        r = requests.post(HOST + "pushes", json.dumps(data), **self._args)
+        return r.json()
 
-    def push_note(self, device, title, body):
-        data = {'type': "note", 'device_iden': device, 'title': title, 'body': body}
-        return self._request(HOST + "/pushes", data)
+    def push_link(self, title, url, device=None):
+        data = {'type': 'link', 'title': title, 'url': url}
+        if device:
+            data["device_iden"] = device
+        r = requests.post(HOST + "pushes", json.dumps(data), **self._args)
+        return r.json()
 
-    def push_address(self, device, name, address):
-        data = {'type': 'address', 'device_iden': device, 'name': name, 'address': address}
-        return self._request(HOST + "/pushes", data)
+    def push_file(self, device, filepath):
+        data = {'type': 'file'}
+        if device:
+            data["device_iden"] = device
+        with open(filepath, "rb") as f:
+            r = requests.post(HOST + "pushes", json.dumps(data), files=[f], **self._args)
 
-    def push_list(self, device, title, items):
-        data = {'type': 'list', 'device_iden': device, 'title': title, 'items': items}
-        return self._request(HOST + "/pushes", data)
-
-    def push_link(self, device, title, url):
-        data = {'type': 'link', 'device_id': device, 'title': title, 'url': url}
-        return self._request(HOST + "/pushes", data)
-
-    def push_file(self, device, file):
-        data = {'type': 'file', 'device_id': device}
-        with open(file, "rb") as f:
-            filedata = f.read()
-        return self._request_multiform(HOST + "/pushes", data, [('file', os.path.basename(file), filedata)])
+        return r.json()

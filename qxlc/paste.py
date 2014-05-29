d2f81923c99ef8385d16e4b7236e57905de95973
Dabo Ross <daboross@daboross.net>
2014-05-29 14:00:21 -0700

    Don't log paste info, no longer needed

--- qxlc/paste.py
+++ qxlc/paste.py
@@ -37,7 +37,6 @@ def action_paste():
     if not "paste" in request.form:
         return "Missing form data: paste", 400
     data = request.form["paste"].strip()
-    logging.info("Got data {}".format(data))
     raw_id = store_data("paste", hashlib.md5(data.encode()).hexdigest())  # use md5sum as data to detect duplicates
     filepath = os.path.join(paste_path, str(raw_id))
     if not os.path.exists(filepath):
